5714af11220b9bf9137c06a67a7e3acd3bb7383b
Markus W. Weissmann <mail@mweissmann.de>
2016-07-29 07:28:44 +0200

    fix module-prefix

--- examples/mcp23017.ml
+++ examples/mcp23017.ml
@@ -16,18 +16,18 @@ let _ =
   in
 
   (* configure bank A and B as output *)
-  let () = get_ok (I2c.SMBus.write_word_data i2c gpioa_conf (Stdint.Uint16.of_int 0x0000)) in
-  let () = get_ok (I2c.SMBus.write_word_data i2c gpiob_conf (Stdint.Uint16.of_int 0x0000)) in
+  let () = get_ok (I2c.write_word_data i2c gpioa_conf (Stdint.Uint16.of_int 0x0000)) in
+  let () = get_ok (I2c.write_word_data i2c gpiob_conf (Stdint.Uint16.of_int 0x0000)) in
 
   (* write to bank A and B *)
   let rec loop () =
     let () = print_endline "setting bank A to all 1, all B to all 0" in
-    let () = get_ok (I2c.SMBus.write_word_data i2c gpioa_addr (Stdint.Uint16.of_int 0xFFFF)) in
-    let () = get_ok (I2c.SMBus.write_word_data i2c gpiob_addr (Stdint.Uint16.of_int 0x0000)) in
+    let () = get_ok (I2c.write_word_data i2c gpioa_addr (Stdint.Uint16.of_int 0xFFFF)) in
+    let () = get_ok (I2c.write_word_data i2c gpiob_addr (Stdint.Uint16.of_int 0x0000)) in
     let () = Unix.sleep 1 in
     let () = print_endline "setting bank A to all 0, all B to all 1" in
-    let () = get_ok (I2c.SMBus.write_word_data i2c gpioa_addr (Stdint.Uint16.of_int 0x0000)) in
-    let () = get_ok (I2c.SMBus.write_word_data i2c gpiob_addr (Stdint.Uint16.of_int 0xFFFF)) in
+    let () = get_ok (I2c.write_word_data i2c gpioa_addr (Stdint.Uint16.of_int 0x0000)) in
+    let () = get_ok (I2c.write_word_data i2c gpiob_addr (Stdint.Uint16.of_int 0xFFFF)) in
     let () = Unix.sleep 1 in
     loop ()
   in
