ecd9825ce7e2ec923a741fa2d4d47dea04c20d26
Virgile Robles <virgile.robles@protonmail.ch>
2023-01-21 14:30:43 +0100

    Convert to ctypes

--- lib/i2c.mli
+++ lib/i2c.mli
@@ -1,17 +1,15 @@
-type t = private Unix.file_descr
+type t
+type 'a result = ('a, [ `EUnix of Unix.error ]) Result.t
 
-val set_address : Unix.file_descr -> int -> (t, [> `EUnix of Unix.error ]) Result.result
-val write_quick : t -> Stdint.uint8 -> (unit, [> `EUnix of Unix.error ]) Result.result
-val read_byte : t -> (Stdint.uint8, [> `EUnix of Unix.error ]) Result.result
-val write_byte : t -> Stdint.uint8 -> (unit, [> `EUnix of Unix.error ]) Result.result
-val read_byte_data : t -> Stdint.uint8 -> (Stdint.uint8, [> `EUnix of Unix.error ]) Result.result
-val write_byte_data : t -> Stdint.uint8 -> Stdint.uint8 -> (unit, [> `EUnix of Unix.error ]) Result.result
-val read_word_data : t -> Stdint.uint8 -> (Stdint.uint16, [> `EUnix of Unix.error ]) Result.result
-val write_word_data : t -> Stdint.uint8 -> Stdint.uint16 -> (unit, [> `EUnix of Unix.error ]) Result.result
-val process_call : t -> Stdint.uint8 -> Stdint.uint16 -> (Stdint.uint16, [> `EUnix of Unix.error ]) Result.result
-val read_block_data : t -> Stdint.uint8 -> (Stdint.uint8 list, [> `EUnix of Unix.error ]) Result.result
-val write_block_data : t -> Stdint.uint8 -> Stdint.uint8 list -> (unit, [> `EUnix of Unix.error ]) Result.result
-val read_i2c_block_data : t -> Stdint.uint8 -> Stdint.uint8 -> Stdint.uint8 -> (Stdint.uint8 list, [> `EUnix of Unix.error ]) Result.result
-val write_i2c_block_data : t -> Stdint.uint8 -> Stdint.uint8 list -> (unit, [> `EUnix of Unix.error ]) Result.result
-val block_process_call : t -> Stdint.uint8 -> Stdint.uint8 list -> (Stdint.uint8 list, [> `EUnix of Unix.error ]) Result.result
+val open_device : string -> int -> t result
 
+open Stdint
+
+val read_byte : t -> uint8 result
+val write_byte : t -> uint8 -> unit result
+val write_quick : t -> uint8 -> unit result
+val read_byte_data : t -> uint8 -> uint8 result
+val write_byte_data : t -> uint8 -> uint8 -> unit result
+val read_word_data : t -> uint8 -> uint16 result
+val write_word_data : t -> uint8 -> uint16 -> unit result
+val process_call : t -> uint8 -> uint16 -> uint16 result
